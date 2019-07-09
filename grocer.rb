19357f5944b56cdf06166a389e8bb14a3ec841f9
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-09 21:13:02 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,14 @@
 def consolidate_cart(cart)
-  # code here
+ count = 0
+while cart[count] do |item|
+  newarr = []
+  if newarr !include item 
+    item.push(newarr)
+  else 
+    item = nil 
+  
+  end
+  cart
 end
 
 def apply_coupons(cart, coupons)
