0a1c05f7156a003b0faa6351fc244f62c59a52b2
Martin Crabtree <martin.crabtree@gmail.com>
2019-07-11 22:44:22 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,14 +1,35 @@
 def consolidate_cart(cart)
-  # code here
+  final_cart = {}
+  
+  cart.each do |item|
+    item.each do |name, price_info|
+        if(final_cart[name].nil?)
+          final_cart[name] = price_info.merge({:count => 1})
+        else
+          final_cart[name][count:]+=1 
+        end
+    end
+  end
+  
+  return final_cart
 end
+
+
+
 
 def apply_coupons(cart, coupons)
   # code here
 end
 
+
+
+
 def apply_clearance(cart)
   # code here
 end
+
+
+
 
 def checkout(cart, coupons)
   # code here
